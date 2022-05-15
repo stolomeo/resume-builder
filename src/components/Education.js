e499cff16e6aea3738819cbd83a8aecbd991e59f
stolomeo <tolomeosebastian@gmail.com>
2022-05-15 00:48:12 -0400

    add education section

---
+++ src/components/Education.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Education() {
+  return (
+    <section>
+      <h2>Experience</h2>
+      <form>
+        <input type="text" name="univserity" placeholder="University" />
+        <input type="text" name="city" placeholder="City" />
+        <input type="text" name="degree" placeholder="Degree" />
+        <input type="text" name="subject" placeholder="Subject" />
+        <input type="text" name="From" placeholder="From" />
+        <input type="text" name="To" placeholder="To" />
+      </form>
+    </section>
+  );
+}
