0bbabe881dab6a657b1b5c053ea374e7c9bcbd9b
stolomeo <tolomeosebastian@gmail.com>
2022-05-18 22:31:41 -0400

    convert resume objects to single array

--- src/components/Utils/resume.js
+++ src/components/Utils/resume.js
@@ -1,28 +1,33 @@
-import { nanoid } from "nanoid";
-
-export const personalItems = {
-  firstName: "John",
-  lastName: "Smith",
-  email: "john@smith.com",
-  phone: "123-456-7890",
-  homeCity: "Boca Raton",
-  homeState: "FL",
-};
-export const experienceItems = {
-  id: nanoid(),
-  employerName: "Apple",
-  jobTitle: "Software Engineer",
-  schoolCity: "Miami",
-  schoolState: "FL",
-  startDate: "March 2020",
-  endDate: "April 2020",
-};
-export const educationItems = {
-  id: nanoid(),
-  university: "FAU",
-  degree: "Bachelors of Science ",
-  workCity: "Boca Raton",
-  major: "Computer Science",
-  workState: "FL",
-  graduationDate: "May 2020",
-};
+const resume = [
+  {
+    personalItems: {
+      university: "FAU",
+      degree: "Bachelors of Science ",
+      workCity: "Boca Raton",
+      major: "Computer Science",
+      workState: "FL",
+      graduationDate: "May 2020",
+    },
+  },
+  {
+    experienceItems: {
+      employerName: "Apple",
+      jobTitle: "Software Engineer",
+      schoolCity: "Miami",
+      schoolState: "FL",
+      startDate: "March 2020",
+      endDate: "April 2020",
+    },
+  },
+  {
+    educationItems: {
+      university: "FAU",
+      degree: "Bachelors of Science ",
+      workCity: "Boca Raton",
+      major: "Computer Science",
+      workState: "FL",
+      graduationDate: "May 2020",
+    },
+  },
+];
+export default resume;
