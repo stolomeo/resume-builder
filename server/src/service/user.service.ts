129cc1effd3392c4b99e0d3c496e1bc31f3c8b7f
stolomeo <tolomeosebastian@gmail.com>
2022-06-08 21:49:32 -0400

    Create user service functions

---
+++ server/src/service/user.service.ts
@@ -0,0 +1,17 @@
+import { getDb } from "../gateway/mongo";
+
+type User = {
+  username: string;
+  password: string;
+};
+
+export const getUserCollection = async () => {
+  const db = await getDb();
+  return db.collection("users");
+};
+
+export const createUser = async (user: User) => {
+  const col = await getUserCollection();
+  const { insertedId } = await col.insertOne(user);
+  return insertedId.toString();
+};
