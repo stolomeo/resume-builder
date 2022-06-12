760b502a666c018d564ddcf1f0f20e3b283346d6
stolomeo <tolomeosebastian@gmail.com>
2022-06-11 20:10:17 -0400

    Styled home page

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,4 +1,3 @@
-import MenuIcon from "@mui/icons-material/Menu";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -10,21 +9,27 @@ export default function Nav() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar sx={{ backgroundColor: "white", gap: "1rem" }}>
           <IconButton
+            href="/"
             size="large"
             edge="start"
-            color="inherit"
+            color="success"
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            Resume Builder
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Temp
-          </Typography>
-          <Button color="inherit" href="/login">
-            Login
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, color: "black" }}
+          ></Typography>
+          <Button color="primary" variant="outlined" href="/login" size="large">
+            Sign in
+          </Button>
+          <Button color="primary" variant="outlined" href="/login" size="large">
+            Get Started
           </Button>
         </Toolbar>
       </AppBar>
