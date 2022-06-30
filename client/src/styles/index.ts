1262a7b843e838c8b89586695aea777f161bb7b1
stolomeo <tolomeosebastian@gmail.com>
2022-06-30 18:40:36 -0400

    Clean

--- client/src/styles/index.ts
+++ client/src/styles/index.ts
@@ -1,6 +1,39 @@
-export const UserAuth = {
+export const userForm = {
   marginTop: 8,
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
+};
+
+export const avatar = {
+  m: 1,
+  bgcolor: "secondary.main",
+};
+
+export const resumeContainer = {
+  display: "flex",
+  mt: 1.5,
+  flexDirection: "column",
+  height: "100%",
+  overflow: "auto",
+  "@media print": {
+    display: "block",
+    width: "100%",
+    height: "100%",
+    position: "fixed",
+    top: 0,
+    left: 0,
+  },
+};
+
+export const personalBox = {
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
+export const resumeSectionBox = {
+  display: "flex",
+  alignItems: "flex-end",
 };
